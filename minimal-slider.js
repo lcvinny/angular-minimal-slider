ed6334787fcef03cb61ae4299d93dabe2a1971f6
Vinh Hoi Le Chau <mail@lcvinny.de>
2013-12-20 17:06:09 +0100

    added touch support use css transforms

--- minimal-slider.js
+++ minimal-slider.js
@@ -1,9 +1,27 @@
 angular.module('minimalSlider', [])
 	.directive('minimalSlider', function($document){
-		var start = 'touchstart mousedown';
-		var end = 'touchend touchcancel mouseup';
-		var move = 'touchmove mousemove';
 		
+		var setPos = function(scope, elem, eventX){
+			var slider = angular.element(elem.children()[0]);
+			var thumb = angular.element(slider.children()[0]);
+			var sliderWidth = slider.prop('offsetWidth');
+			var thumbWidth = thumb.prop('offsetWidth');
+			var sliderLeft = slider.prop('offsetLeft');
+			var thumbLeft = thumb.prop('offsetLeft');
+
+			var pos = eventX - sliderLeft;
+			pos = Math.min(Math.max(pos, 0), sliderWidth);
+			var translate = 'translate(' + (pos - thumbWidth / 2) + 'px, 0px)'
+
+			thumb.css('transform', translate);
+			thumb.css('-ms-transform', translate);
+			thumb.css('-webkit-transform', translate);
+			
+			scope.model = pos / sliderWidth;
+			scope.$apply()
+			scope.change()
+		}
+
 		return {
 			restrict: 'E',
 			scope: {
@@ -12,24 +30,21 @@ angular.module('minimalSlider', [])
 			},
 			template: '<div class="minimal-slider"><div class="minimal-slider-thumb"></div></div>',
 			link: function(scope, elem, attr, ctrl){
-				var slider = angular.element(elem.children()[0]);
-				var thumb = angular.element(slider.children()[0]);
-				var sliderWidth = slider.prop('offsetWidth');
-				var thumbWidth = thumb.prop('offsetWidth');
-				var sliderLeft = slider.prop('offsetLeft');
-				var thumbLeft = thumb.prop('offsetLeft');
-				elem.on(start, function(event){
-					$document.on(move, function(event){
-						var pos = event.pageX - sliderLeft;
-						pos = Math.min(Math.max(pos, 0), sliderWidth);
-						thumb.css('left', (pos - thumbWidth / 2) + 'px');
-						scope.model = pos / sliderWidth;
-						scope.$apply()
-						scope.change()
+				elem.on('touchstart', function(event){
+					setPos(scope, elem, event.changedTouches[0].pageX);
+					$document.on('touchmove', function(event){
+						event.preventDefault();
+						setPos(scope, elem, event.changedTouches[0].pageX);
 					});
 				});
-				$document.on(end, function(event){
-					$document.off(move);
+				elem.on('mousedown', function(event){
+					setPos(scope, elem, event.pageX);
+					$document.on('mousemove', function(event){
+						setPos(scope, elem, event.pageX);
+					});
+				});
+				$document.on('touchend touchcancel mouseup', function(event){
+					$document.off('touchmove mousemove');
 				});				
 			}
 		}
