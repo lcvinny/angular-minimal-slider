2425a93dc0ec17abdf5b93d17adf3052f70be0eb
Vinh Hoi Le Chau <mail@lcvinny.de>
2014-03-24 18:13:57 +0100

    using hammer.js for better touch support added attribute support larger controls for touch devices using rem units thumb slides inside wilder only

--- minimal-slider.js
+++ minimal-slider.js
@@ -3,8 +3,10 @@ angular.module('minimalSlider', [])
 
 		var setRatio = function(scope, elem, eventX){
 			var slider = angular.element(elem.children()[0]);
-			var sliderWidth = slider.prop('offsetWidth');
-			var sliderLeft = elem.children()[0].getBoundingClientRect().left;
+			var thumb = angular.element(slider.children()[0]);
+			var thumbWidth = thumb.prop('offsetWidth');
+			var sliderWidth = slider.prop('offsetWidth') - thumbWidth;
+			var sliderLeft = elem.children()[0].getBoundingClientRect().left + thumbWidth / 2;
 			var pos = eventX - sliderLeft;
 			pos = Math.min(Math.max(pos, 0), sliderWidth);
 			scope.model = pos / sliderWidth;
@@ -16,11 +18,11 @@ angular.module('minimalSlider', [])
 		var setPosition = function(scope, elem){
 			var slider = angular.element(elem.children()[0]);
 			var thumb = angular.element(slider.children()[0]);
-			var sliderWidth = slider.prop('offsetWidth');
 			var thumbWidth = thumb.prop('offsetWidth');
-
+			var sliderWidth = slider.prop('offsetWidth') - thumbWidth;
+			
 			var pos = scope.model * sliderWidth;
-			var translate = 'translate(' + (pos - thumbWidth / 2) + 'px, 0px)'
+			var translate = 'translate(' + (pos) + 'px, 0px)'
 
 			thumb.css('transform', translate);
 			thumb.css('-ms-transform', translate);
@@ -33,32 +35,36 @@ angular.module('minimalSlider', [])
 		}
 
 		return {
-			restrict: 'E',
+			restrict: 'E A',
 			scope: {
 				model: '=',
 				change: '&'		
 			},
 			template: '<div class="minimal-slider"><div class="minimal-slider-thumb"></div></div>',
 			link: function(scope, elem, attr, ctrl){
+
+				var slider = angular.element(elem.children()[0])[0];
+				var thumb = angular.element(angular.element(elem.children()[0]).children()[0])[0];
+
+				if(typeof $window.hammer == 'undefined'){
+					$window.hammer = Hammer($window.document.body,{
+						prevent_default: true
+					});
+				}
+				$window.hammer.on('drag', function(event){
+					if(event.gesture.startEvent.target == thumb){
+						setRatioAndPosition(scope, elem, event.gesture.center.pageX);	
+					}
+				}).on('tap', function(event){
+					if(event.gesture.target == slider){
+						setRatioAndPosition(scope, elem, event.gesture.center.pageX);	
+					}
+				});	
+				
+
 				angular.element($window).on('resize', function(){
 					setPosition(scope, elem);
 				});
-				elem.on('touchstart', function(event){
-					setRatioAndPosition(scope, elem, event.changedTouches[0].pageX);
-					$document.on('touchmove', function(event){
-						event.preventDefault();
-						setRatioAndPosition(scope, elem, event.changedTouches[0].pageX);
-					});
-				});
-				elem.on('mousedown', function(event){
-					setRatioAndPosition(scope, elem, event.pageX);
-					$document.on('mousemove', function(event){
-						setRatioAndPosition(scope, elem, event.pageX);
-					});
-				});
-				$document.on('touchend touchcancel mouseup', function(event){
-					$document.off('touchmove mousemove');
-				});				
 			}
 		}
 	});
